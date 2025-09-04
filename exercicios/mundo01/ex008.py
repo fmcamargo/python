38aa12812a7ca7b7d84a741a2338516f64a7dbb0
fmcamargo <moisesfabio94@gmail.com>
2025-09-03 22:28:46 -0300

    Exercício do curso de Python do Guanabara

--- exercicios/mundo01/ex008.py
+++ exercicios/mundo01/ex008.py
@@ -2,3 +2,15 @@
 Escreva um programa que leia um valor em METROS e mostre o valor
 convertido em CENTIMETROS e MILIMETROS
 '''
+
+distMetros = float(input('Digite uma distância em metros: '))
+
+#Centímetros
+distCent = distMetros*100
+
+#Milímetros
+distMili = distMetros*1000
+
+#Saídas das conversões
+print(f'A distância de {distMetros}m convertida para centímetros é {distCent}cm.')
+print(f'A distância de {distMetros}m convertida para milímetros é {distMili}mm.')
