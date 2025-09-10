f055c25e0cf567d8fda4aba0d6547241724b81df
fmcamargo <moisesfabio94@gmail.com>
2025-09-09 21:45:48 -0300

    Resolução do ex009 do Curso em Vídeo

--- exercicios/mundo01/ex009.py
+++ exercicios/mundo01/ex009.py
@@ -2,3 +2,11 @@
 Faça um programa que leia um número INTEIRO qualquer e mostre em tela a sua tabuada
 '''
 
+numero = int(input('Digite um número: '))
+
+#regra para definir um range que inicia em 1 e vai até 10
+for multiplicador in range (1,11):
+    #variável que pega o número digitado e multiplica pelo valor dentro do range
+    resultado = numero * multiplicador
+    #saída formatada em modelo de tabuada
+    print(f'{numero} x {multiplicador:2} = {resultado}')
