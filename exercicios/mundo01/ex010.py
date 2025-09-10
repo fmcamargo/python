fae7224f1250c947093a558a8d8df17f1cb71df2
fmcamargo <moisesfabio94@gmail.com>
2025-09-09 22:14:47 -0300

    Resolução do ex010

--- exercicios/mundo01/ex010.py
+++ exercicios/mundo01/ex010.py
@@ -1,6 +1,15 @@
 '''
-Crie um progrma que leia quanto dinheiro tem na carteira e mostre quiantos dólares ela pode comprar
-
-Cotação dólar hoje = 5,47
+Crie um progrma que leia quanto dinheiro tem na carteira e mostre quantos dólares se pode comprar
 '''
 
+#cotação que tem no google
+dolarHoje = 5.43
+
+#valor float porque é dinheiro
+carteira = float(input('Quanto você tem na carteira?\nR$ '))
+
+#converter moeda do valor que tem dividido por outra variável
+cotacao = carteira / dolarHoje
+
+#sáida formatada com as moedas
+print(f'Com R$ {carteira:.2f} você pode comprar US$ {cotacao:.2f}')
